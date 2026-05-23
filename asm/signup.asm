08cb54aff808faf2228b29c6d0c706b5b9f60c57
KASHIF HUSSAIN <f2024-0791@bnu.edu.pk>
2026-05-23 18:26:54 +0500

    Update signup.asm

--- asm/signup.asm
+++ asm/signup.asm
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Signup</title>
+<link rel="stylesheet" href="style.css">
+</head>
 
+<body>
+
+<h1>Create Account</h1>
+
+<form>
+
+<input
+type="text"
+placeholder="Username">
+
+<br><br>
+
+<input
+type="password"
+placeholder="Password">
+
+<br><br>
+
+<button>
+Sign Up
+</button>
+
+</form>
+
+</body>
+</html>
